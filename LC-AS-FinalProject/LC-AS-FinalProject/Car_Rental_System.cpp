46251d7ba3642e92d490ff928947889aa9b3ca5e
laynechant <layne.chant@gmail.com>
2024-04-22 10:34:32 -0400

    Added Classes

---
+++ LC-AS-FinalProject/LC-AS-FinalProject/Car_Rental_System.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include  "CAR_RENTAL_SYSTEM.h"
+using namespace std; 
+
+
+class User
+{
+public:
+    void printInfo() {};
+
+
+private: 
+    
+
+
+};
+
+class Shopper : public User
+{
+
+};
+
+class Admin
+{
+
+};
+
+class Car
+{
+
+};
+
+
+int main()
+{
+    cout << "Welcome to this Car Rental System" << endl; 
+    
+}
+
